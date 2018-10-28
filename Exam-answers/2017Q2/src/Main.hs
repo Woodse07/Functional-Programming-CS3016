cf4bd66213317dd04e79b059ad85009a74f2880d
Woodse07 <sewoods@tcd.ie>
2018-10-28 17:08:55 +0000

    Finished -part b but noticed problem with f4, come back to this

--- Exam-answers/2017Q2/src/Main.hs
+++ Exam-answers/2017Q2/src/Main.hs
@@ -2,3 +2,12 @@
 hof :: a -> (a -> a -> a) -> [a] -> a
 hof x fx [] = x
 hof x fx (y:ys) = y `fx` hof x fx ys
+
+-- b
+f1 = hof 42 (*)
+f2 = hof 0 f2x 
+	where x `f2x` y = (99 * y)
+f3 = hof 0 (+)
+f4 = hof [] (++)
+f5 = hof 0 f5x
+	where x `f5x` y = (x-42) + y
