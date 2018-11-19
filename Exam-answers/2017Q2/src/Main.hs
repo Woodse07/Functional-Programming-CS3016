bb8ddb24061d017aa86f8ffdccc3bb9115e06c04
Woodse07 <sewoods@tcd.ie>
2018-11-19 15:48:56 +0000

    Finished Assignment 02

--- Exam-answers/2017Q2/src/Main.hs
+++ Exam-answers/2017Q2/src/Main.hs
@@ -13,3 +13,9 @@ f5 = hof 0 f5x
 	where x `f5x` y = (x-42) + y
 
 -- c
+{-
+
+Few place where this function will fail.. when checking a leaf node the function only cheacks if x is equal to the node, if this isn't true, and it might'nt be, then we will have a runtime error. Also when searching a tree the nodes to the right are never travesed, so if the node we are looking for is to the right we will never find it and get a runtime error. We also never check if a non-leaf node is x. Basically if the node isn't the bottom left node we get a runtime error. 
+
+-}
+
