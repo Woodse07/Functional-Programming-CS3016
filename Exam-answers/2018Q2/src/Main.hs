f2cd074d4c7cf1420650923f17d5cf74b235168c
Woodse07 <sewoods@tcd.ie>
2018-12-05 16:31:24 +0000

    Finished 2018 q2

--- Exam-answers/2018Q2/src/Main.hs
+++ Exam-answers/2018Q2/src/Main.hs
@@ -1,5 +1,53 @@
-module Main where
+--a
+{-
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+The first thing I notice is that regardless if x is less than or greater than i, the function
+only ever searches to the right. So if the value you are searching for is greater than i, it
+will never be found.
+
+When checking if a leaf, the function only checks if it is the correct value. If it was not
+we would get a runtime error.
+
+Doesn't check for empty tree?
+
+
+-}
+
+--b
+{-
+data Tree = Empty
+			| Single Int String
+			| Many Tree Int String Tree
+
+search :: Int -> Tree -> Maybe String
+
+search x (Single i s)
+	| x == i = Just s
+	| otherwise = Nothing
+
+search x (Many left i s right)
+	| x == i = Just s
+	| x > i = search x left
+	| x > i = search x right
+-}
+
+--c
+{-
+data Tree = Empty
+			| Single Int String
+			| Many Tree Int String Tree
+
+search :: (Monad m) => Int -> Tree -> m String
+
+search x Empty = fail "Empty tree"
+
+search x (Single i s)
+	| x == i = return s
+	| otherwise = error "Not in leaf"
+
+search x (Many left i s right)
+	| x == i = return s
+	| x > i = search x left
+	| x > i = search x right
+-}
+
